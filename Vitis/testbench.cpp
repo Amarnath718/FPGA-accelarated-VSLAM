bbfebdab98f684fc3f5466b03fd22a410cf60aa8
Amarnath B <amarjaganb@gmail.com>
2026-03-10 21:03:03 +0530

    Add testbench for depthwise and pointwise kernels

---
+++ Vitis/testbench.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include "dsc_block.h"
+
+int main()
+{
+
+static float input[CIN][H][W];
+static float depth_kernel[CIN][K][K];
+static float point_kernel[COUT][CIN];
+static float output[COUT][H][W];
+
+
+// initialize input
+for(int c=0;c<CIN;c++)
+{
+    for(int i=0;i<H;i++)
+    {
+        for(int j=0;j<W;j++)
+        {
+            input[c][i][j] = 1;
+        }
+    }
+}
+
+
+// initialize depthwise kernel
+for(int c=0;c<CIN;c++)
+{
+    for(int i=0;i<K;i++)
+    {
+        for(int j=0;j<K;j++)
+        {
+            depth_kernel[c][i][j] = 0.1;
+        }
+    }
+}
+
+
+// initialize pointwise kernel
+for(int oc=0;oc<COUT;oc++)
+{
+    for(int ic=0;ic<CIN;ic++)
+    {
+        point_kernel[oc][ic] = 0.1;
+    }
+}
+
+
+// run block
+dsc_block(input,depth_kernel,point_kernel,output);
+
+
+// print output sample
+std::cout<<"Output sample: "
+         <<output[0][10][10]
+         <<std::endl;
+
+return 0;
+
+}
