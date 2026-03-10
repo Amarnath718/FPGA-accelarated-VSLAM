4d59d785476fc80a53d25ec81bb0a0dff7345b02
Amarnath B <amarjaganb@gmail.com>
2026-03-10 21:02:09 +0530

    Add header file for DSC block function

---
+++ Vitis/dsc_block.h
@@ -0,0 +1,17 @@
+#ifndef DSC_BLOCK_H
+#define DSC_BLOCK_H
+
+#define CIN 32
+#define COUT 64
+#define H 32
+#define W 32
+#define K 3
+
+void dsc_block(
+float input[CIN][H][W],
+float depth_kernel[CIN][K][K],
+float point_kernel[COUT][CIN],
+float output[COUT][H][W]
+);
+
+#endif
